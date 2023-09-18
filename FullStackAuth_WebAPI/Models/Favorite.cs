a57ed7ff01aaa47bbc03c5e499b34cfc3083b1e3
bmaselli13 <brian.maselli@gmail.com>
2023-09-18 11:08:30 -0700

    Added Favorite Model

---
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using FullStackAuth_WebAPI.Models;
+
+namespace BookNookBackend.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
